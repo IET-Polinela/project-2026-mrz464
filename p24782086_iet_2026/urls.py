c4ac756cbf3b412e186c25abd0bd4264bfb6c79e
Muhammad Rezaldi Zazky <rezaldizazky06@gmail.com>
2026-03-13 00:28:19 +0700

    menambahkan urls agar bisa terhubung ke apk

--- p24782086_iet_2026/urls.py
+++ p24782086_iet_2026/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse # Import wajib untuk Lab 1
-
-# Ini fungsi untuk menampilkan tulisan
-def welcome_view(request):
-    return HttpResponse("Selamat Datang")
+from django.urls import path, include # Import include sangat penting di sini [cite: 23]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome/', welcome_view), # Ini rute untuk memanggil tulisan tersebut
+    # Menghubungkan URL project dengan URL masing-masing app [cite: 8, 25]
+    path('', include('main_app.urls')),      # Halaman utama [cite: 25]
+    path('about/', include('about.urls')),   # Halaman about [cite: 38]
+    path('contacts/', include('contacts.urls')), # Halaman contacts [cite: 38]
 ]
